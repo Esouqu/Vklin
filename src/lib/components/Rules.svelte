633a2de36a90358ec024bdc57977a2d1f2028a8c
Esouqu <nippl3juic3@gmail.com>
2025-02-28 19:56:26 +0700

    update

--- src/lib/components/Rules.svelte
+++ src/lib/components/Rules.svelte
@@ -2,7 +2,17 @@
 	import { Button } from './ui/button';
 </script>
 
-<Button href="https://telegra.ph/Pravila-zakazov-igr-dlya-donaterov-02-14" target="_blank">
+<Button
+	href="https://docs.google.com/forms/d/e/1FAIpQLSebg5S9RICT4g9ozi5fz-EvGJRRUuV1quV5O513Yh8nSdArRw/viewform"
+	target="_blank"
+>
+	Предложить идею
+</Button>
+<Button
+	variant="outline"
+	href="https://telegra.ph/Pravila-zakazov-igr-dlya-donaterov-02-14"
+	target="_blank"
+>
 	Для донатеров
 </Button>
 <Button variant="outline" href="https://telegra.ph/Pravila-02-13-139" target="_blank">
